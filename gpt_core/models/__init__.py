dc1aed1a1b0377f791c622c70b9304ad9767362f
Fernando Menchaca Villanueva <63605207+SinkableAbyss26@users.noreply.github.com>
2025-08-19 19:57:11 -0600

    feat(core): add gpt core service module

---
+++ gpt_core/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019-Present InTechual Solutions. (<https://intechualsolutions.com/>)
+
+from . import chatgpt_model
+from . import res_config_settings
+from . import gpt_service
+from . import gpt_completion_log
---
+++ gpt_core/models/gpt_completion_log.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from odoo import fields, models
+
+
+class GPTCompletionLog(models.Model):
+    """Store usage and cost details for GPT API calls."""
+    _name = 'gpt.completion.log'
+    _description = 'GPT Completion Log'
+    _order = 'id desc'
+
+    model_name = fields.Char(string='Model')
+    prompt_tokens = fields.Integer()
+    completion_tokens = fields.Integer()
+    total_tokens = fields.Integer()
+    cost = fields.Float(help='Approximate cost in USD')
+    currency = fields.Char(default='USD')
+    prompt = fields.Text()
+    response = fields.Text()
