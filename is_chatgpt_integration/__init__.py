51a978f7c6c17326a6c1b4f65780a89a327f4114
Fernando Menchaca <fmenchacav@gmail.com>
2025-08-19 19:41:54 -0600

    importacion de modulo base

---
+++ is_chatgpt_integration/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019-Present InTechual Solutions. (<https://intechualsolutions.com/>)
+
+from . import controllers
+from . import models
---
+++ is_chatgpt_integration/controllers/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019-Present InTechual Solutions. (<https://intechualsolutions.com/>)
+
+from . import main
